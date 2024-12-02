3cef9369d661d442445f6ae05bfb3d5ac2c2d29b
Riccardo De Cesaris <ric.dc@hotmail.it>
2024-12-02 12:40:46 +0100

    Out-of-context files Removed

--- HI-SSL/run.sh
+++
@@ -1,3 +0,0 @@
-pip install -r requirements.txt
-python main.py -dir=./ -td=./data_dir/train -vd=./data_dir/val -c=config
-python main_test.py -dir=./ -td=./data_dir/train -vd=./data_dir/test -c=config
