4e15cb58e79d1ab3f83c27fb74d7b98917f47558
Riccardo De Cesaris <ric.dc@hotmail.it>
2024-12-14 19:18:44 +0100

    Modulization of the Repository: "log"

--- log/setup_exp.py
+++ log/setup_exp.py
@@ -1,6 +1,8 @@
 import os, json
 from argparse import ArgumentParser
 
+LOG_ROOT = "./log"
+
 ### ################# ###
 ### SUPPORT FUNCTIONS ###
 ### ################# ###
@@ -24,7 +26,10 @@ def validate_and_process_args(args) -> dict:
     test_id = args.test_id
     
     # Check if the Metadata JSON file for the given 'test_id' already exists
-    if os.path.exists(f"./{test_id}-metadata.json"): 
+    # if os.path.exists(f"./{test_id}-metadata.json"): 
+    #     print("*** IN RELATION TO THE SPECIFIED EXPERIMENT, THE METADATA FILE HAS ALREADY BEEN CREATED! ***\n")
+    #     exit(1)
+    if os.path.exists(f"{LOG_ROOT}/{test_id}-metadata.json"): 
         print("*** IN RELATION TO THE SPECIFIED EXPERIMENT, THE METADATA FILE HAS ALREADY BEEN CREATED! ***\n")
         exit(1)
     
@@ -50,7 +55,9 @@ def validate_and_process_args(args) -> dict:
     return exp_metadata
 
 def save_metadata(metadata: dict, test_id: str):
-    with open(f"./{test_id}-metadata.json", "w") as jf:
+    # with open(f"./{test_id}-metadata.json", "w") as jf:
+    #     json.dump(metadata, jf, indent=4)
+    with open(f"{LOG_ROOT}/{test_id}-metadata.json", "w") as jf:
         json.dump(metadata, jf, indent=4)
 
 ### #### ###
