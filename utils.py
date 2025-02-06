96411c15fe869eb44cfe0ec3813a322e4585ce21
Riccardo De Cesaris <ric.dc@hotmail.it>
2025-02-06 11:34:46 +0100

    Create utils.py
    Global Utils

---
+++ utils.py
@@ -0,0 +1,48 @@
+import json
+
+LOG_ROOT = "./log"
+
+### ################# ###
+### METADATA HANDLING ###
+### ################# ###
+def load_metadata(metadata_path: str) -> dict:
+    try:
+        with open(metadata_path, 'r') as jf: return json.load(jf)
+    except json.JSONDecodeError as e: 
+        raise ValueError(f"Error occurred while decoding JSON file '{metadata_path}': {e}")
+    except Exception as e: 
+        raise FileNotFoundError(f"Error occurred while reading metadata file '{metadata_path}': {e}")
+
+def save_metadata(metadata: dict, metadata_path: str):
+    with open(metadata_path, 'w') as jf:
+        json.dump(metadata, jf, indent=4)
+
+### ################ ###
+### DATASET HANDLING ###
+### ################ ###
+def get_train_instance_patterns():
+    train_instance_patterns = {
+        "CEDAR_Letter": lambda f: 'c' not in f,
+        "CVL": lambda f: "-3" not in f and "-7" not in f,
+        "VatLat653": lambda f: 'a' in f
+    }
+    
+    return train_instance_patterns
+
+def get_test_instance_patterns():
+    test_instance_patterns = {
+        "CEDAR_Letter": lambda f: 'c' in f,
+        "CVL": lambda f: "-3" in f or "-7" in f,
+        "VatLat653": lambda f: 't' in f
+    }
+    
+    return test_instance_patterns
+
+def get_vert_hor_cuts(dataset):
+    vert_cuts, hor_cuts = None, None
+    
+    if dataset == "CEDAR_Letter": vert_cuts, hor_cuts = 7, 4
+    if dataset == "CVL": vert_cuts, hor_cuts = 5, 2
+    if dataset == "VatLat653": vert_cuts, hor_cuts = 1, 1
+    
+    return vert_cuts, hor_cuts
