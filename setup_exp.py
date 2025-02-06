1149bf590b9e27c99ca0894d9449b9aab2510acf
Riccardo De Cesaris <ric.dc@hotmail.it>
2025-02-06 11:35:59 +0100

    Improvement of "Experiment Setup Phase"

--- setup_exp.py
+++ setup_exp.py
@@ -1,5 +1,7 @@
-import os, json
+import os
 from argparse import ArgumentParser
+
+from utils import save_metadata
 
 LOG_ROOT = "./log"
 
@@ -17,8 +19,6 @@ def get_args():
     parser.add_argument("-dataset", type=str, required=True, choices=['CEDAR_Letter', 'CVL', 'VatLat653'], help="Dataset employed in the experiment")
     parser.add_argument("-classes", type=str, required=True, help="Comma-separated list of classes")
     parser.add_argument("-class_types", type=str, required=True, help="Comma-separated list of class types ('Base'/'Masked')")
-    parser.add_argument("-vert_mult_fact", type=int, default=1, help="Multiplicative factor for vertical cuts")
-    parser.add_argument("-hor_mult_fact", type=int, default=1, help="Multiplicative factor for horizontal cuts")
     
     return parser.parse_args()
 
@@ -26,16 +26,13 @@ def validate_and_process_args(args) -> dict:
     test_id = args.test_id
     
     # Check if the Metadata JSON file for the given 'test_id' already exists
-    if os.path.exists(f"{LOG_ROOT}/{test_id}-metadata.json"): 
-        print("*** IN RELATION TO THE SPECIFIED EXPERIMENT, THE METADATA FILE HAS ALREADY BEEN CREATED! ***\n")
+    exp_metadata_path = os.path.join(LOG_ROOT, f"{test_id}-metadata.json")
+    if os.path.exists(exp_metadata_path): 
+        print(f"*** IN RELATION TO THE EXPERIMENT '{test_id}', THE METADATA FILE HAS ALREADY BEEN CREATED! ***\n")
         exit(1)
     
-    # Parse and validation for the 'classes' and the 'class_types' parameters
-    args_classes, classes = args.classes.split(','), list()
-    for a in args_classes: classes.append(int(a.strip()))
-    
-    args_class_types, class_types = args.class_types.split(','), list()
-    for a in args_class_types: class_types.append(a.strip())
+    classes = [int(c.strip()) for c in args.classes.split(',')]
+    class_types = [ct.strip() for ct in args.class_types.split(',')]
 
     if len(classes) > len(class_types): raise Exception("There is at least one Class without the corresponding Class Type")
     if len(classes) < len(class_types): raise Exception("There is at least one Class Type which cannot be attributed to any Class")
@@ -45,17 +42,10 @@ def validate_and_process_args(args) -> dict:
         "TEST_ID": test_id,
         "MODEL_TYPE": args.model_type,
         "DATASET": args.dataset,
-        "CLASSES": {c: c_type for c, c_type in zip(classes, class_types)},
-        "PREP_MULT_FACT": {
-            "VERT": args.vert_mult_fact,
-            "HOR": args.hor_mult_fact
-        } 
+        "CLASSES": dict(zip(classes, class_types))
     }
 
     return exp_metadata
-
-def save_metadata(metadata: dict, test_id: str):
-    with open(f"{LOG_ROOT}/{test_id}-metadata.json", "w") as jf: json.dump(metadata, jf, indent=4)
 
 ### #### ###
 ### MAIN ###
@@ -63,9 +53,12 @@ def save_metadata(metadata: dict, test_id: str):
 if __name__ == '__main__':
     args = get_args()
     TEST_ID = args.test_id
+    EXP_METADATA_PATH = os.path.join(LOG_ROOT, f"{TEST_ID}-metadata.json")
     
     try:
-        exp_metadata = validate_and_process_args(args)
-        save_metadata(exp_metadata, TEST_ID)
+        EXP_METADATA = validate_and_process_args(args)
+        save_metadata(EXP_METADATA, EXP_METADATA_PATH)
         print(f"*** METADATA SUCCESSFULLY CREATED FOR: {TEST_ID} ***\n")
-    except Exception as e: print(f"Error on Metadata creation for Experiment: {TEST_ID}")
+    except Exception as e: 
+        print(f"Error on Metadata creation for Experiment: {TEST_ID}")
+        print(f"Details: {e}")
