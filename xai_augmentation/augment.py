e9fb0e5e2f19e9480e540f4dafa1a3cd85d4d2fe
Riccardo De Cesaris <ric.dc@hotmail.it>
2025-03-03 18:23:22 +0100

    Massive Update
    - Augmentation Modes: "protect_inform" (pi), "lime_reds" (lr), "world_opening" (wo) and "random" (rand)
    - Training Set balancing

--- xai_augmentation/augment.py
+++ xai_augmentation/augment.py
@@ -1,12 +1,13 @@
 import os, torch
 from argparse import ArgumentParser
+from datetime import datetime
 
-from utils import load_metadata
+from utils import xaiaug2str, str2bool, load_metadata, save_metadata
 
 from classifiers.utils.fine_tune_utils import load_model
 from classifiers.utils.dataloader_utils import Faithfulness_Test_DataLoader, load_rgb_mean_std
 
-from xai_augmentation.augment_utils import retrieve_training_instances, extract_class_mean_vectors, compute_augmentation_rates, extract_augmented_crops
+from xai_augmentation.augment_utils import retrieve_training_instances, setup_dimensionality_reduction, extract_class_mean_vectors, compute_augmentation_rates, extract_augmented_crops
 
 LOG_ROOT = "./log"
 CLASSIFIERS_ROOT = "./classifiers"
@@ -17,6 +18,8 @@ def get_args():
     parser = ArgumentParser()
     parser.add_argument("-test_id", type=str, required=True)
     parser.add_argument("-xai_algorithm", type=str, required=True, choices=["LimeBase"])
+    parser.add_argument("-mode", type=xaiaug2str, required=True)
+    parser.add_argument("-keep_balanced", type=str2bool, default=True)
     
     return parser.parse_args()
                   
@@ -24,10 +27,14 @@ def get_args():
 ### MAIN ###
 ### #### ###
 if __name__ == "__main__":
+    torch.cuda.empty_cache()
     args = get_args()
     
     TEST_ID = args.test_id
     XAI_ALGORITHM = args.xai_algorithm
+    MODE = args.mode
+    KEEP_BALANCED = args.keep_balanced
+
     EXP_METADATA = load_metadata(f"{LOG_ROOT}/{TEST_ID}-metadata.json")
     DATASET = EXP_METADATA["DATASET"]
     MODEL_TYPE = EXP_METADATA["MODEL_TYPE"]
@@ -38,17 +45,24 @@ if __name__ == "__main__":
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     CP = f"{CLASSIFIERS_ROOT}/classifier_{MODEL_TYPE}/cp/Test_3_TL_val_best_model.pth"
     
-    model, _ = load_model(MODEL_TYPE, len(CLASSES_DATA), CP, TEST_ID, EXP_METADATA, DEVICE)
+    model, _ = load_model(MODEL_TYPE, len(CLASSES_DATA), "frozen", CP, "test", TEST_ID, EXP_METADATA, DEVICE)
+
+    root_dir = f"{XAI_AUG_ROOT}/{TEST_ID}/{MODE}_balanced" if KEEP_BALANCED else f"{XAI_AUG_ROOT}/{TEST_ID}/{MODE}_notbalanced"
         
-    root_dir = f"{XAI_AUG_ROOT}/{TEST_ID}"
     os.makedirs(root_dir, exist_ok=True)
     retrieve_training_instances(root_dir, DATASET, CLASSES)
     EXP_DIR = f"{CLASSIFIERS_ROOT}/classifier_{MODEL_TYPE}/tests/{TEST_ID}"
     mean_, std_ = load_rgb_mean_std(EXP_DIR)
     dl = Faithfulness_Test_DataLoader(directory=f"{root_dir}/train_instances", classes=CLASSES, batch_size=1, img_crop_size=CROP_SIZE, mean=mean_, std=std_)
+
+    print(f"*** BEGINNING OF AUGMENTATION PROCESS: MODE '{MODE}', KEEP_BALANCED '{KEEP_BALANCED}' ***")
+    
+    pca = None
+    if MODE in ["pi", "wo"]:
+        pca = setup_dimensionality_reduction(model, dl, DEVICE, final_dim=64)
     
     print("Extracting class mean vectors...")
-    mean_class_vectors, c_to_idx, idx_to_c = extract_class_mean_vectors(model, dl, DEVICE)
+    mean_class_vectors, c_to_idx, idx_to_c = extract_class_mean_vectors(model, dl, pca, DEVICE)
     
     print("Computing augmentation rates...")
     augmentations = compute_augmentation_rates(model, dl, idx_to_c, DEVICE)
@@ -58,8 +72,44 @@ if __name__ == "__main__":
     XAI_EXP_DIR_NAME = EXP_METADATA[f"{XAI_ALGORITHM}_base_METADATA"]["DIR_NAME"]
     P_WIDTH, P_HEIGHT = EXP_METADATA[f"{XAI_ALGORITHM}_base_METADATA"]["PATCH_DIM"]["WIDTH"], EXP_METADATA[f"{XAI_ALGORITHM}_base_METADATA"]["PATCH_DIM"]["HEIGHT"]
     XAI_EXP_DIR = f"{XAI_ROOT}/explanations/patches_{P_WIDTH}x{P_HEIGHT}_removal/{XAI_EXP_DIR_NAME}"
+
+    if KEEP_BALANCED:
+        max_augmentations = 0
+        for c in CLASSES:
+            n_augmentations = augmentations[c_to_idx[str(c)]]
+            if n_augmentations > max_augmentations: max_augmentations = n_augmentations
+        
+        if MODE == "rand":
+            for c in CLASSES: augmentations[c_to_idx[str(c)]] = (0, max_augmentations)
+        else:
+            for c in CLASSES:
+                xai_augmentations = augmentations[c_to_idx[str(c)]]
+                random_augmentations = max_augmentations - xai_augmentations
+                augmentations[c_to_idx[str(c)]] = (xai_augmentations, random_augmentations)
+    
+    else:
+        if MODE == "rand":
+            for c in CLASSES:
+                class_augmentations = augmentations[c_to_idx[str(c)]]
+                augmentations[c_to_idx[str(c)]] = (0, class_augmentations)
+        else:
+            for c in CLASSES:
+                xai_augmentations = augmentations[c_to_idx[str(c)]]
+                augmentations[c_to_idx[str(c)]] = (xai_augmentations, 0)
+
     for c in CLASSES:
-        print(f"Extracting Augmentation crops for class: {c}")
-        mean_class_vector = mean_class_vectors[c_to_idx[str(c)]]
-        n_augmentations = augmentations[c_to_idx[str(c)]]
-        extract_augmented_crops(root_dir, XAI_EXP_DIR, c, c_to_idx, mean_class_vector, n_augmentations, CROP_SIZE, P_WIDTH, P_HEIGHT, mean_, std_, model, DEVICE)
+        xai_and_random_augmentations = augmentations[c_to_idx[str(c)]]
+        extract_augmented_crops(MODE, root_dir, XAI_EXP_DIR, c, c_to_idx, mean_class_vectors, xai_and_random_augmentations, CROP_SIZE, P_WIDTH, P_HEIGHT, mean_, std_, pca, model, DEVICE)
+        
+    xai_aug_metadata_entry = f"XAI_AUGMENTATION_{MODE}_BALANCED" if KEEP_BALANCED else f"XAI_AUGMENTATION_{MODE}"
+    EXP_METADATA[xai_aug_metadata_entry] = dict()
+    EXP_METADATA[xai_aug_metadata_entry]["TIMESTAMP"] = str(datetime.now())
+    EXP_METADATA[xai_aug_metadata_entry]["INSTANCES"] = dict()
+    for c in CLASSES:
+        EXP_METADATA[xai_aug_metadata_entry]["INSTANCES"][f"class_{c}"] = dict()
+        EXP_METADATA[xai_aug_metadata_entry]["INSTANCES"][f"class_{c}"]["XAI_AUG_CROPS"] = augmentations[c_to_idx[str(c)]][0]
+        EXP_METADATA[xai_aug_metadata_entry]["INSTANCES"][f"class_{c}"]["RANDOM_AUG_CROPS"] = augmentations[c_to_idx[str(c)]][1]
+    save_metadata(EXP_METADATA, f"{LOG_ROOT}/{TEST_ID}-metadata.json")
+    os.system(f"rm -r {root_dir}/train_instances")
+    
+    print(f"*** END OF AUGMENTATION PROCESS WITH: MODE '{MODE}', KEEP_BALANCED '{KEEP_BALANCED}' ***\n")
--- xai_augmentation/augment_utils.py
+++ xai_augmentation/augment_utils.py
@@ -4,6 +4,7 @@ import torchvision.transforms as T
 import torch.nn.functional as F
 from PIL import Image
 from tqdm import tqdm
+from sklearn.decomposition import PCA
 
 from utils import get_train_instance_patterns
 
@@ -29,7 +30,30 @@ def retrieve_training_instances(root_dir, dataset, classes):
             source, dest = f"{dataset_dir}/processed/{c}/{f}", f"{root_dir}/train_instances/{c}/{f}"
             os.system(f"cp {source} {dest}")
 
-def extract_class_mean_vectors(model, dl, device):
+def setup_dimensionality_reduction(model, dl, device, final_dim):
+    pca, pca_input = PCA(n_components=final_dim), None
+    dataset, set_ = dl.load_data()
+    
+    total_crops, crop_counter = None, 0
+    for data, _ in tqdm(set_, desc="Processing Training Instances to setup PCA-dimensionality reducer"):
+        data = data.to(device)
+        _, ncrops, c, h, w = data.size()
+
+        with torch.no_grad():
+            vis_features = model.extract_visual_features(data.view(-1, c, h, w))
+            if pca_input is None:
+                total_crops = len(dataset) * ncrops
+                pca_input = np.zeros((total_crops, vis_features.size()[1]))
+                
+            for crop_idx in range(0, vis_features.size()[0]):
+                pca_input[crop_counter] = vis_features[crop_idx].cpu().numpy()
+                crop_counter += 1
+    
+    pca.fit(pca_input)
+
+    return pca
+    
+def extract_class_mean_vectors(model, dl, pca, device):
     dataset, set_ = dl.load_data()
     c_to_idx = dataset.class_to_idx
     idx_to_c = {c_to_idx[k]: k for k in list(c_to_idx.keys())}
@@ -43,12 +67,17 @@ def extract_class_mean_vectors(model, dl, device):
         
         with torch.no_grad():
             vis_features = model.extract_visual_features(data.view(-1, c, h, w))
-            for crop_idx in range(0, len(vis_features)):
+            for crop_idx in range(0, vis_features.size()[0]):
                 mean_vectors[target.item()] += vis_features[crop_idx].cpu().numpy()
             num_crops[target.item()] += ncrops
     
     for c in idx_to_c.keys(): mean_vectors[c] /= num_crops[c]
-    
+
+    if pca is not None:
+        for c in idx_to_c.keys():
+            reduced_mean_vector = pca.transform(mean_vectors[c].reshape(1, -1))
+            mean_vectors[c] = reduced_mean_vector[0]
+
     return mean_vectors, c_to_idx, idx_to_c
 
 def compute_augmentation_rates(model, dl, idx_to_c, device):
@@ -81,15 +110,76 @@ def produce_augmented_crops(root_dir, results, c, mean_):
         mean_int = tuple(int(m * 255) for m in mean_)
         crop = T.Pad(pad_coordinates, fill=mean_int)(crop)
 
-        crop.save(f"{root_dir}/crops_for_augmentation/{c}/{instance_name[:-4]}_crop{i}.jpg")
+        crop.save(f"{root_dir}/crops_for_augmentation/{c}/{instance_name[:-4]}_crop{i}{instance_name[-4:]}")
 
-def extract_augmented_crops(root_dir, xai_exp_dir, c, c_to_idx, mean_vector, n_augmentations, crop_size, patch_width, patch_height, mean_, std_, model, device):
-    os.makedirs(f"{root_dir}/crops_for_augmentation/{c}", exist_ok=True)
+def produce_random_augmented_crops(root_dir, c, crop_size, random_augmentations):
+    instance_names = os.listdir(f"{root_dir}/train_instances/{c}")
+    crops2instance = {k: 0 for k in instance_names}
+    
+    while random_augmentations > 0:
+        for k in instance_names:
+            crops2instance[k] += 1
+            random_augmentations -= 1
+            if random_augmentations == 0: break
+    
+    for k in instance_names:
+        img = Image.open(f"{root_dir}/train_instances/{c}/{k}")
+        img_w, img_h = img.size
+        crops_to_extract = crops2instance[k]
+        for i in range(0, crops_to_extract):
+            left = np.random.randint(low=0, high=img_w - crop_size + 1)
+            top = np.random.randint(low=0, high=img_h - crop_size + 1)
+            right, bottom = left + crop_size, top + crop_size
+            
+            crop = img.crop((left, top, right, bottom))
+            crop.save(f"{root_dir}/crops_for_augmentation/{c}/{k[:-4]}_random_crop{i}{k[-4:]}")
+            
+def get_crop_from_patch(crop_size, patch_width, patch_height, left_b_patch, top_b_patch, right_b_patch, bottom_b_patch):
+    left_b_crop, top_b_crop, right_b_crop, bottom_b_crop = 0, 0, 0, 0
+    left_b_crop_to_pad, top_b_crop_to_pad, right_b_crop_to_pad, bottom_b_crop_to_pad = 0, 0, 0, 0
+    
+    crop_left_top_semisize, crop_right_bottom_semisize = 0, 0
+    if crop_size % 2 == 0: crop_left_top_semisize, crop_right_bottom_semisize = int(crop_size/2), int(crop_size/2)
+    else: crop_left_top_semisize, crop_right_bottom_semisize = int(crop_size/2), int(crop_size/2)+1
+    
+    patch_left_semisize, patch_right_semisize = 0, 0
+    if patch_width % 2 == 0: patch_left_semisize, patch_right_semisize = int(patch_width/2), int(patch_width/2)
+    else: patch_left_semisize, patch_right_semisize = int(patch_width/2), int(patch_width/2)+1
+    
+    patch_top_semisize, patch_bottom_semisize = 0, 0
+    if patch_height % 2 == 0: patch_top_semisize, patch_bottom_semisize = int(patch_height/2), int(patch_height/2)
+    else: patch_top_semisize, patch_bottom_semisize = int(patch_height/2), int(patch_height/2)+1
+    
+    left_difference = crop_left_top_semisize - patch_left_semisize
+    top_difference = crop_left_top_semisize - patch_top_semisize
+    right_difference = crop_right_bottom_semisize - patch_right_semisize
+    bottom_difference = crop_right_bottom_semisize - patch_bottom_semisize
+    
+    if left_b_patch - left_difference >= 0: left_b_crop = left_b_patch - left_difference
+    else: left_b_crop_to_pad = left_difference - left_b_patch
+    
+    if top_b_patch - top_difference >= 0: top_b_crop = top_b_patch - top_difference
+    else: top_b_crop_to_pad = top_difference - top_b_patch
+    
+    if right_b_patch + right_difference <= 902: right_b_crop = right_b_patch + right_difference
+    else: 
+        right_b_crop = 902
+        right_b_crop_to_pad = right_difference - (902 - right_b_patch)
+    
+    if bottom_b_patch + bottom_difference <= 1279: bottom_b_crop = bottom_b_patch + bottom_difference
+    else: 
+        bottom_b_crop = 1279
+        bottom_b_crop_to_pad = bottom_difference - (1279 - bottom_b_patch)
+    
+    return (left_b_crop, top_b_crop, right_b_crop, bottom_b_crop), (left_b_crop_to_pad, top_b_crop_to_pad, right_b_crop_to_pad, bottom_b_crop_to_pad)
+
+def extract_augmented_crops_protect_inform(root_dir, xai_exp_dir, c, c_to_idx, mean_vectors, crop_size, patch_width, patch_height, mean_, std_, pca, model, device):  
     instance_names = os.listdir(f"{root_dir}/train_instances/{c}")
     mask = Image.open(f"{XAI_ROOT}/def_mask_{patch_width}x{patch_height}.png")
     mask_array = np.array(mask)
     label = c_to_idx[str(c)]
-    
+    mean_class_vector = mean_vectors[c_to_idx[str(c)]]
+
     results = []
     
     for name in tqdm(instance_names):
@@ -111,33 +201,12 @@ def extract_augmented_crops(root_dir, xai_exp_dir, c, c_to_idx, mean_vector, n_a
                         idx += 1
                         continue
                     
-                    bottom_b_patch, right_b_patch = bottom_b_patch + 1, right_b_patch + 1
-                    left_b_crop, top_b_crop, right_b_crop, bottom_b_crop = 0, 0, 0, 0
-                    left_b_crop_to_pad, top_b_crop_to_pad, right_b_crop_to_pad, bottom_b_crop_to_pad = 0, 0, 0, 0
+                    right_b_patch, bottom_b_patch = right_b_patch + 1, bottom_b_patch + 1
+                    crop_coordinates, pad_coordinates = get_crop_from_patch(crop_size, patch_width, patch_height, left_b_patch, top_b_patch, right_b_patch, bottom_b_patch)
                     
-                    if left_b_patch - 165 >= 0: left_b_crop = left_b_patch - 165
-                    else:
-                        left_b_crop = 0
-                        left_b_crop_to_pad = 165 - left_b_patch
-                    
-                    if top_b_patch - 165 >= 0: top_b_crop = top_b_patch - 165
-                    else:
-                        top_b_crop = 0
-                        top_b_crop_to_pad = 165 - top_b_patch
-                    
-                    if right_b_patch + 165 <= 902: right_b_crop = right_b_patch + 165
-                    else:
-                        right_b_crop = 902
-                        right_b_crop_to_pad = 165 - (902 - right_b_patch)
-                    
-                    if bottom_b_patch + 165 <= 1279: bottom_b_crop = bottom_b_patch + 165
-                    else:
-                        bottom_b_crop = 1279
-                        bottom_b_crop_to_pad = 165 - (1279 - bottom_b_patch)
-                    
-                    crop = img.crop((left_b_crop, top_b_crop, right_b_crop, bottom_b_crop))
+                    crop = img.crop(crop_coordinates)
                     mean_int = tuple(int(m * 255) for m in mean_)
-                    crop = T.Pad((left_b_crop_to_pad, top_b_crop_to_pad, right_b_crop_to_pad, bottom_b_crop_to_pad), fill=mean_int)(crop)
+                    crop = T.Pad(pad_coordinates, fill=mean_int)(crop)
                     
                     tensor_crop = T.ToTensor()(crop)
                     normalized_tensor_crop = T.Normalize(mean_, std_)(tensor_crop)
@@ -147,21 +216,154 @@ def extract_augmented_crops(root_dir, xai_exp_dir, c, c_to_idx, mean_vector, n_a
                         vis_f = model.extract_visual_features(normalized_tensor_crop.unsqueeze(0)).cpu().numpy()
                         out = model(normalized_tensor_crop.unsqueeze(0))
                         probs = F.softmax(out, dim=1).squeeze(0).cpu().numpy()
+                    
+                    vis_f = pca.transform(vis_f.reshape(1, -1))[0]
                         
-                    cosine = np.dot(vis_f, mean_vector) / (np.linalg.norm(vis_f) * np.linalg.norm(mean_vector))
-                    utility = np.exp(1-probs[label]) * cosine
+                    cosine = np.dot(vis_f, mean_class_vector) / (np.linalg.norm(vis_f) * np.linalg.norm(mean_class_vector))
+                    pi_utility = np.exp(1-probs[label]) * cosine
                     
                     results.append({
                         "instance": name,
-                        "utility": float(utility),
+                        "utility": float(pi_utility),
                         "patch_coordinates": tuple(map(int, (left_b_patch, top_b_patch, right_b_patch, bottom_b_patch))),
-                        "crop_coordinates": tuple(map(int, (left_b_crop, top_b_crop, right_b_crop, bottom_b_crop))),
-                        "pad_coordinates": tuple(map(int, (left_b_crop_to_pad, top_b_crop_to_pad, right_b_crop_to_pad, bottom_b_crop_to_pad)))
+                        "crop_coordinates": tuple(map(int, crop_coordinates)),
+                        "pad_coordinates": tuple(map(int, pad_coordinates))
                     })
     
-    results = sorted(results, key=lambda x: x['utility'], reverse=True)
-    with open(f"{root_dir}/crops_for_augmentation/{c}_crops_for_augmentations.json", "w") as f:
-        json.dump(results, f, indent=4)
+    return results
+
+def extract_augmented_crops_lime_reds(root_dir, xai_exp_dir, c, crop_size, patch_width, patch_height):    
+    instance_names = os.listdir(f"{root_dir}/train_instances/{c}")
+    mask = Image.open(f"{XAI_ROOT}/def_mask_{patch_width}x{patch_height}.png")
+    mask_array = np.array(mask)
+
+    results = []
+
+    for name in tqdm(instance_names):
+        img = Image.open(f"{root_dir}/train_instances/{c}/{name}")
+        inst_name = name[:-4]
+        with open(f"{xai_exp_dir}/{inst_name}/{inst_name}_scores.pkl", "rb") as f:
+            base_scores = pickle.load(f)
+        reduced_scores = reduce_scores(mask, base_scores)
+        
+        for idx, score in reduced_scores.items():
+            if score != [np.nan] and score < 0:
+                positions = np.argwhere(mask_array == idx)
+                if positions.size > 0:
+                    top_b_patch, left_b_patch = positions.min(axis=0)
+                    bottom_b_patch, right_b_patch = positions.max(axis=0)
+                    
+                    if ((bottom_b_patch - top_b_patch + 1)) != patch_height and (right_b_patch - left_b_patch + 1) != patch_width:
+                        print(f"Skipped patch {idx} for instance {inst_name} because of wrong dimensions")
+                        idx += 1
+                        continue
+                    
+                    right_b_patch, bottom_b_patch = right_b_patch + 1, bottom_b_patch + 1
+                    crop_coordinates, pad_coordinates = get_crop_from_patch(crop_size, patch_width, patch_height, left_b_patch, top_b_patch, right_b_patch, bottom_b_patch)
+                    
+                    left_b_crop, top_b_crop, right_b_crop, bottom_b_crop = crop_coordinates
+                    mask_crop = mask_array[left_b_crop:right_b_crop+1, top_b_crop:bottom_b_crop+1]
+                    
+                    crop_area = (right_b_crop - left_b_crop) * (bottom_b_crop - top_b_crop)
+                    mask_crop_idxs = np.unique(mask_crop)
+                    total_attribution_score = 0
+                    
+                    for elem in mask_crop_idxs:
+                        idx_area = np.sum(mask_crop_idxs == elem)
+                        if reduced_scores[elem] != [np.nan]: total_attribution_score += reduced_scores[elem] * idx_area
+                        else: continue
+                        
+                    lr_utility = -1 * total_attribution_score / crop_area
+                    
+                    results.append({
+                        "instance": name,
+                        "utility": float(lr_utility),
+                        "patch_coordinates": tuple(map(int, (left_b_patch, top_b_patch, right_b_patch, bottom_b_patch))),
+                        "crop_coordinates": tuple(map(int, crop_coordinates)),
+                        "pad_coordinates": tuple(map(int, pad_coordinates))
+                    })
     
-    results = results[:n_augmentations]
-    produce_augmented_crops(root_dir, results, c, mean_)
+    return results
+
+def extract_augmented_crops_world_opening(root_dir, xai_exp_dir, c, c_to_idx, mean_vectors, crop_size, patch_width, patch_height, mean_, std_, pca, model, device):
+    instance_names = os.listdir(f"{root_dir}/train_instances/{c}")
+    mask = Image.open(f"{XAI_ROOT}/def_mask_{patch_width}x{patch_height}.png")
+    mask_array = np.array(mask)
+    label = c_to_idx[str(c)]
+
+    results = []
+    
+    for name in tqdm(instance_names):
+        img = Image.open(f"{root_dir}/train_instances/{c}/{name}")
+        inst_name = name[:-4]
+        with open(f"{xai_exp_dir}/{inst_name}/{inst_name}_scores.pkl", "rb") as f:
+            base_scores = pickle.load(f)
+        reduced_scores = reduce_scores(mask, base_scores)
+        
+        for idx, score in reduced_scores.items():
+            if score != [np.nan] and score < 0:
+                positions = np.argwhere(mask_array == idx)
+                if positions.size > 0:
+                    top_b_patch, left_b_patch = positions.min(axis=0)
+                    bottom_b_patch, right_b_patch = positions.max(axis=0)
+                    
+                    if ((bottom_b_patch - top_b_patch + 1)) != patch_height and (right_b_patch - left_b_patch + 1) != patch_width:
+                        print(f"Skipped patch {idx} for instance {inst_name} because of wrong dimensions")
+                        idx += 1
+                        continue
+                    
+                    right_b_patch, bottom_b_patch = right_b_patch + 1, bottom_b_patch + 1
+                    crop_coordinates, pad_coordinates = get_crop_from_patch(crop_size, patch_width, patch_height, left_b_patch, top_b_patch, right_b_patch, bottom_b_patch)
+                    
+                    crop = img.crop(crop_coordinates)
+                    mean_int = tuple(int(m * 255) for m in mean_)
+                    crop = T.Pad(pad_coordinates, fill=mean_int)(crop)
+                    
+                    tensor_crop = T.ToTensor()(crop)
+                    normalized_tensor_crop = T.Normalize(mean_, std_)(tensor_crop)
+                    normalized_tensor_crop = normalized_tensor_crop.to(device)
+                    
+                    with torch.no_grad(): vis_f = model.extract_visual_features(normalized_tensor_crop.unsqueeze(0)).cpu().numpy()
+                    vis_f = pca.transform(vis_f.reshape(1, -1))[0]
+                    
+                    euc_different_classes = 0
+                    for k in mean_vectors.keys():
+                        if k != label: euc_different_classes += np.linalg.norm(vis_f - mean_vectors[k])
+                    mean_euc_different_classes = euc_different_classes / (len(mean_vectors) - 1)
+                    euc_true_class = np.linalg.norm(vis_f - mean_vectors[label])
+                    wo_utility = mean_euc_different_classes - euc_true_class
+                    
+                    results.append({
+                        "instance": name,
+                        "utility": float(wo_utility),
+                        "patch_coordinates": tuple(map(int, (left_b_patch, top_b_patch, right_b_patch, bottom_b_patch))),
+                        "crop_coordinates": tuple(map(int, crop_coordinates)),
+                        "pad_coordinates": tuple(map(int, pad_coordinates))
+                    })
+    
+    return results
+        
+def extract_augmented_crops(mode, root_dir, xai_exp_dir, c, c_to_idx, mean_vectors, xai_and_random_augmentations, crop_size, patch_width, patch_height, mean_, std_, pca, model, device):
+    os.makedirs(f"{root_dir}/crops_for_augmentation/{c}", exist_ok=True)
+    xai_augmentations, random_augmentations = xai_and_random_augmentations
+    
+    if xai_augmentations > 0:
+        print(f"Extracting {xai_augmentations} XAI-Augmentation Crops for class {c}")
+        results = None
+        if mode == "pi": results = extract_augmented_crops_protect_inform(root_dir, xai_exp_dir, c, c_to_idx, mean_vectors, crop_size, patch_width, patch_height, mean_, std_, pca, model, device)
+        elif mode == "lr": results = extract_augmented_crops_lime_reds(root_dir, xai_exp_dir, c, crop_size, patch_width, patch_height)
+        elif mode == "wo": results = extract_augmented_crops_world_opening(root_dir, xai_exp_dir, c, c_to_idx, mean_vectors, crop_size, patch_width, patch_height, mean_, std_, pca, model, device)
+    
+        results = sorted(results, key=lambda x: x['utility'], reverse=True)
+        with open(f"{root_dir}/crops_for_augmentation/{c}_crops_for_augmentations.json", "w") as f:
+            json.dump(results, f, indent=4)
+    
+        results = results[:xai_augmentations]
+        produce_augmented_crops(root_dir, results, c, mean_)
+    else: print(f"Skipping extraction of XAI-Augmentation Crops for class {c}: no Crops have to be extracted in this way")
+    
+    if random_augmentations > 0:
+        print(f"Extracting {random_augmentations} Random-Augmentation Crops for class {c}")
+        produce_random_augmented_crops(root_dir, c, crop_size, random_augmentations)
+    
+    else: print(f"Skipping extraction of Random-Augmentation Crops for class {c}: no Crops have to be extracted in this way")
