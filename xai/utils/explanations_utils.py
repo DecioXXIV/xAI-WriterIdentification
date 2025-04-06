40ca37c4d151108a75f5a43d724fc44714afb34e
Riccardo De Cesaris <ric.dc@hotmail.it>
2025-04-06 18:35:20 +0200

    Update explanations_utils.py
    Correction on thr Explainability Process: Attribution Score aggregation (if "iters > 1"), normalization and reduction

--- xai/utils/explanations_utils.py
+++ xai/utils/explanations_utils.py
@@ -7,13 +7,10 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 from matplotlib.colors import LinearSegmentedColormap
 from tqdm import tqdm
 
-from concurrent.futures import ThreadPoolExecutor
-from collections import defaultdict
-
 from utils import get_train_instance_patterns, get_test_instance_patterns
 
 from xai.explainers.lime_base_explainer import LimeBaseExplainer
-from xai.explainers.glime_binomial_explainer import GLimeBinomialExplainer
+# from xai.explainers.glime_binomial_explainer import GLimeBinomialExplainer
 from xai.utils.image_utils import create_image_grid
 
 XAI_ROOT = "./xai"
@@ -38,22 +35,29 @@ def get_instances_to_explain(dataset, source, class_to_idx, phase):
 
     return instances, labels
 
-def reduce_and_normalize_scores(scores, reduction_method="mean"):
+def normalize_scores(scores, percentile=99):
+    keys, values = list(scores.keys()), np.array(list(scores.values()))
+    sorted_scores = {k: v for k, v in sorted(scores.items(), key=lambda item: item[1], reverse=False)}
+    sorted_values = np.array(list(sorted_scores.values()))
+    
+    cum_sums = np.cumsum(np.abs(sorted_values))
+    threshold_id = np.where(cum_sums >= cum_sums[-1] * 0.01 * percentile)[0][0]
+    threshold = sorted_values[threshold_id]
+    
+    norm_values = np.clip(values / threshold, -1, 1)
+    norm_scores = {k: v for k, v in zip(keys, norm_values)}
+    
+    return norm_scores
+
+def reduce_scores(scores, reduction_method="mean"):
     reductions = {"mean": np.mean, "median": np.median}
     red_func = reductions.get(reduction_method, np.mean)
 
     reduced_scores = dict()
-    for k in scores.keys(): reduced_scores[k] = float(red_func(scores[k]))
+    for k in scores.keys(): reduced_scores[str(k)] = float(red_func(scores[k]))
     
-    norm_scores = dict()
-    attributions = np.array(list(reduced_scores.values()))
-    min_val, max_val = np.min(attributions), np.max(attributions)
-    norm_attributions = 2 * (attributions - min_val) / (max_val - min_val) - 1
-
-    for k in reduced_scores.keys(): norm_scores[k] = float(norm_attributions[k])
-
-    return norm_scores
-
+    return reduced_scores
+    
 def assign_attr_scores_to_mask(base_mask, scores):
     base_mask_array = np.array(deepcopy(base_mask)).astype(np.float32)
 
@@ -65,8 +69,8 @@ def assign_attr_scores_to_mask(base_mask, scores):
 def setup_explainer(xai_algorithm, surrogate_model, model_type, model, num_samples, kernel_width, mean, std):
     if xai_algorithm == "LimeBase":
         return LimeBaseExplainer(model_type, model, surrogate_model, mean, std, num_samples, kernel_width)
-    elif xai_algorithm == "GLimeBinomial":
-        return GLimeBinomialExplainer(model_type, model, surrogate_model, mean, std, num_samples, kernel_width)
+    # elif xai_algorithm == "GLimeBinomial":
+    #     return GLimeBinomialExplainer(model_type, model, surrogate_model, mean, std, num_samples, kernel_width)
 
 def explain_instance(explainer, img, img_rows, img_cols, instance_name, label, mask, mask_rows, mask_cols, output_dir, crop_size, overlap, iters):
     scores = dict()
@@ -81,64 +85,31 @@ def explain_instance(explainer, img, img_rows, img_cols, instance_name, label, m
                 crop_img, crop_mask = img.crop(coordinates), mask.crop(coordinates)
                 crop_mask_array = np.array(crop_mask)
                 segments = np.array(crop_mask_array/100, dtype=np.uint16)
+                
+                crop_attr = dict()
 
-                for i in range(0, iters):
-                    attr_scores = explainer.explain_instance(crop_img, segments, label)
-                    for k, v in attr_scores.items():
-                        if k not in scores: scores[k] = v
-                        else: scores[k].extend(v)
+                for iteration in range(0, iters):
+                    attr = explainer.explain_instance(crop_img, segments, label)
+                    for k, v in attr.items():
+                        if k not in crop_attr: crop_attr[k] = v
+                        else: crop_attr[k] += v
+                
+                for k, v in crop_attr.items(): crop_attr[k] = float(v / iters)
+                
+                normalized_crop_attr = normalize_scores(crop_attr, percentile=98)
+                
+                for k, v in normalized_crop_attr.items(): scores[k].append(v)
                 
                 pbar.update(1)
 
-    norm_scores = reduce_and_normalize_scores(scores)
-    with open(f"{output_dir}/{instance_name}_scores.json", "w") as f: json.dump(norm_scores, f, indent=4)
+    final_scores = reduce_scores(scores)
+    with open(f"{output_dir}/{instance_name}_scores.json", "w") as f: json.dump(final_scores, f, indent=4)
 
-    return norm_scores
-
-# def process_crop(explainer, crop_img, crop_mask, label, iters):
-#     crop_mask_array = np.array(crop_mask)
-#     segments = (crop_mask_array / 100).astype(np.uint16)
-    
-#     local_scores = defaultdict(list)
-#     for _ in range(iters):
-#         attr_scores = explainer.explain_instance(crop_img, segments, label)
-#         for k, v in attr_scores.items():
-#             local_scores[k].extend(v)
-    
-#     return local_scores
-
-# def explain_instance(explainer, img, img_rows, img_cols, instance_name, label, mask, 
-#                       mask_rows, mask_cols, output_dir, crop_size, overlap, iters):
-    
-#     scores = defaultdict(list)
-#     grid_dict = create_image_grid(img_rows, img_cols, crop_size, overlap)
-    
-#     with tqdm(total=img_rows * img_cols, desc="Crop Processing") as pbar, ThreadPoolExecutor() as executor:
-#         futures = []
-        
-#         for r in range(img_rows):
-#             for c in range(img_cols):
-#                 coordinates = grid_dict[f"{r}_{c}"]
-#                 crop_img, crop_mask = img.crop(coordinates), mask.crop(coordinates)
-                
-#                 futures.append(executor.submit(process_crop, explainer, crop_img, crop_mask, label, iters))
-            
-#         for future in futures:
-#             local_scores = future.result()
-#             for k, v in local_scores.items():
-#                 scores[k].extend(v)
-#             pbar.update(1)
-
-#     norm_scores = reduce_and_normalize_scores(scores)
-    
-#     with open(f"{output_dir}/{instance_name}_scores.json", "w") as f:
-#         json.dump(norm_scores, f, indent=4)
-
-#     return norm_scores
+    return final_scores
 
 def visualize_exp_outcome(scores, mask, instance_name, output_dir,):
     mask_array = np.array(mask)/100
-    for k in scores.keys(): mask_array[mask_array == k] = scores[k]
+    for k in scores.keys(): mask_array[mask_array == float(k)] = scores[k]
 
     fig, ax = plt.subplots(figsize=(6, 6))
     ax.axis("off")
