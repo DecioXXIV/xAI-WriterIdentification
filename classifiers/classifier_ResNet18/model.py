31a3faee6a8a1d533f51106d3681669844851e71
Riccardo De Cesaris <ric.dc@hotmail.it>
2025-04-22 01:02:35 +0200

    Update model.py
    Corrections

--- classifiers/classifier_ResNet18/model.py
+++ classifiers/classifier_ResNet18/model.py
@@ -107,8 +107,8 @@ class ResNet18_Classifier(nn.Module):
         
         new_layer = add_fc_layer('hidden', 1024, 128)
         class_layer = add_fc_layer('last', 128, self.num_classes)
-        self.fc_layers.add_module('ch0', new_layer)
-        self.fc_layers.add_module('ch1', class_layer)
+        self.fc_layers.add_module('fc0', new_layer)
+        self.fc_layers.add_module('fc1', class_layer)
     
     def extract_visual_features(self, x):
         x = self.base_model.enc(x)
