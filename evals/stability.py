03a827d3eabe7d75391980294b1d70c09a1dfe54
Riccardo De Cesaris <ric.dc@hotmail.it>
2025-04-25 23:31:41 +0200

    Final Implementation of "Stability" Evaluation

--- evals/stability.py
+++ evals/stability.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser
 
 from utils import load_metadata, get_logger
 
-from evals.utils.stability_utils import get_instances, get_faith_evaluated_xai_modes, collect_all_instances_attr_scores_per_patch_parallel, get_mode_pairs, produce_instances_abs_differences_report_parallel
+from evals.utils.stability_utils import get_instances, get_faith_evaluated_xai_modes, collect_all_instances_attr_scores_per_patch_parallel, get_mode_pairs, produce_instances_abs_differences_report_parallel, compute_global_abs_differences_stats, produce_instances_jaccard_report_parallel
 
 LOG_ROOT = "./log"
 DATASETS_ROOT = "./datasets"
@@ -15,11 +15,8 @@ def get_args():
     parser.add_argument("-test_id", type=str, required=True)
     parser.add_argument("-xai_algorithm", type=str, required=True, choices=["LimeBase", "GLimeBinomial"])
     parser.add_argument("-surrogate_model", type=str, required=True, choices=["Ridge"])
-    # parser.add_argument("-xai_modes", type=str, default=None)
-    ### "xai_modes": if 'None' then all modes are involved, otherwise it exploits the given modes (list)
-    
-    # parser.add_argument("-stability_eval_name", type=str, required=True)
-    ### "stability_eval_name": useful to perform different evaluations on the same (test_id, xai_algorithm, surrogate_model)
+    parser.add_argument("-xai_modes", type=str, default=None)
+    parser.add_argument("-stability_eval_name", type=str, required=True)
 
     return parser.parse_args()
 
@@ -39,23 +36,32 @@ if __name__ == '__main__':
         exit(1)
     
     XAI_ALGORITHM, SURROGATE_MODEL = args.xai_algorithm, args.surrogate_model
+    XAI_MODES = args.xai_modes
+    STABILITY_EVAL_NAME = args.stability_eval_name
     
     DATASET = EXP_METADATA["DATASET"]
     CLASSES = list(EXP_METADATA["CLASSES"].keys())
     
+    logger.info(f"*** BEGINNING OF STABILITY EVALUATION FOR ['{TEST_ID}', '{XAI_ALGORITHM}', '{SURROGATE_MODEL}'] ***")
+    
     os.makedirs(f"{EVAL_ROOT}/stability/{TEST_ID}", exist_ok=True)
     instances = get_instances(DATASET, CLASSES)
-    for inst in instances: os.makedirs(f"{EVAL_ROOT}/stability/{TEST_ID}/instances/{inst}", exist_ok=True)
+    for inst in instances: os.makedirs(f"{EVAL_ROOT}/stability/{TEST_ID}/{STABILITY_EVAL_NAME}/instances/{inst}", exist_ok=True)
     
-    modes_mapping = get_faith_evaluated_xai_modes(TEST_ID, XAI_ALGORITHM, SURROGATE_MODEL, logger)
-    with open(f"{EVAL_ROOT}/stability/{TEST_ID}/modes_mapping.json", 'w') as jf: json.dump(modes_mapping, jf, indent=4)
+    logger.info("Producing Reports on Absolute Patch Attribution Scores Differences for all instances...")
     
-    collect_all_instances_attr_scores_per_patch_parallel(TEST_ID, instances, modes_mapping, EXP_METADATA)
+    modes_mapping = get_faith_evaluated_xai_modes(TEST_ID, XAI_ALGORITHM, SURROGATE_MODEL, XAI_MODES, logger)
+    with open(f"{EVAL_ROOT}/stability/{TEST_ID}/{STABILITY_EVAL_NAME}/modes_mapping.json", 'w') as jf: json.dump(modes_mapping, jf, indent=4)
+    
+    collect_all_instances_attr_scores_per_patch_parallel(TEST_ID, instances, modes_mapping, EXP_METADATA, STABILITY_EVAL_NAME)
     logger.info("Collected all instances' attribute scores per patch")
     
     mode_pairs = get_mode_pairs(modes_mapping)
-    
-    produce_instances_abs_differences_report_parallel(TEST_ID, instances, modes_mapping, mode_pairs)
+    produce_instances_abs_differences_report_parallel(TEST_ID, instances, modes_mapping, mode_pairs, STABILITY_EVAL_NAME)
     logger.info("Produced absolute differences reports for all instances")
     
-    ### TO IMPLEMENT: Jaccard Reports ###
+    compute_global_abs_differences_stats(TEST_ID, instances, STABILITY_EVAL_NAME)
+    
+    logger.info("Producing Reports on Jaccard Similarity for all instances...")
+    produce_instances_jaccard_report_parallel(TEST_ID, instances, modes_mapping, mode_pairs, STABILITY_EVAL_NAME, logger)
+    logger.info(f"*** END OF STABILITY EVALUATION FOR ['{TEST_ID}', '{XAI_ALGORITHM}', '{SURROGATE_MODEL}'] ***")
--- evals/utils/stability_utils.py
+++ evals/utils/stability_utils.py
@@ -16,24 +16,32 @@ def get_instances(dataset, classes):
     
     return instances
 
-def get_faith_evaluated_xai_modes(test_id, xai_algorithm, surrogate_model, logger):
+def get_faith_evaluated_xai_modes(test_id, xai_algorithm, surrogate_model, xai_modes, logger):
     modes_mapping = dict()
-    modes = list()
-    for mode in os.listdir(f"{EVAL_ROOT}/faithfulness/{test_id}"):
-        if xai_algorithm in mode: modes.append(mode)
+    
+    if xai_modes is None:
+        modes = list()
+        for mode in os.listdir(f"{EVAL_ROOT}/faithfulness/{test_id}"):
+            if xai_algorithm in mode: modes.append(mode)
+    
+    else:
+        xai_modes = [mode.strip() for mode in xai_modes.split(',')]
+        modes = list()
+        for mode in xai_modes: modes.append(f"{xai_algorithm}_{mode}_{surrogate_model}")
+    
     logger.warning(f"These are the ['{xai_algorithm}', '{surrogate_model}'] modes that were evaluated with Faithfulness")
     logger.warning(f"{modes}")
     
     for i in range(0, len(modes)): modes_mapping[modes[i]] = f"mode{i}"
     return modes_mapping
 
-def collect_all_instances_attr_scores_per_patch_parallel(test_id, instances, modes_mapping, exp_metadata):
-    args = [(test_id, inst, modes_mapping, exp_metadata) for inst in instances]
+def collect_all_instances_attr_scores_per_patch_parallel(test_id, instances, modes_mapping, exp_metadata, stability_eval_name):
+    args = [(test_id, inst, modes_mapping, exp_metadata, stability_eval_name) for inst in instances]
     with mp.Pool(mp.cpu_count()) as pool:
         pool.map(process_instance_attr_scores_per_patch, args)
 
 def process_instance_attr_scores_per_patch(args):
-    test_id, inst, modes_mapping, exp_metadata = args
+    test_id, inst, modes_mapping, exp_metadata, stability_eval_name = args
     
     if not os.path.exists(f"{EVAL_ROOT}/stability/{test_id}/instances/{inst}/{inst}_all_mode_scores.csv"):
         df = pd.DataFrame()
@@ -48,23 +56,22 @@ def process_instance_attr_scores_per_patch(args):
             df[mode] = attr_scores
         
         df.set_index("patches", inplace=True)
-        df.to_csv(f"{EVAL_ROOT}/stability/{test_id}/instances/{inst}/{inst}_all_mode_scores.csv", index=True, header=True)
+        df.to_csv(f"{EVAL_ROOT}/stability/{test_id}/{stability_eval_name}/instances/{inst}/{inst}_all_mode_scores.csv", index=True, header=True)
 
 def get_mode_pairs(modes_mapping):
     modes = list(modes_mapping.keys())
     return list(combinations(modes, 2))
 
-def produce_instances_abs_differences_report_parallel(test_id, instances, modes_mapping, mode_pairs):
-    pass
-    args = [(test_id, inst, modes_mapping, mode_pairs) for inst in instances]
+def produce_instances_abs_differences_report_parallel(test_id, instances, modes_mapping, mode_pairs, stability_eval_name):
+    args = [(test_id, inst, modes_mapping, mode_pairs, stability_eval_name) for inst in instances]
     with mp.Pool(mp.cpu_count()) as pool:
         pool.map(process_instance_abs_differences, args)
 
 def process_instance_abs_differences(args):
-    test_id, inst, modes_mapping, mode_pairs = args
+    test_id, inst, modes_mapping, mode_pairs, stability_eval_name = args
     
-    if not os.path.exists(f"{EVAL_ROOT}/stability/{test_id}/instances/{inst}/{inst}_abs_differences.csv"):
-        all_mode_scores = pd.read_csv(f"{EVAL_ROOT}/stability/{test_id}/instances/{inst}/{inst}_all_mode_scores.csv", index_col=0, header=0)
+    if not os.path.exists(f"{EVAL_ROOT}/stability/{test_id}/{stability_eval_name}/instances/{inst}/{inst}_abs_differences.csv"):
+        all_mode_scores = pd.read_csv(f"{EVAL_ROOT}/stability/{test_id}/{stability_eval_name}/instances/{inst}/{inst}_all_mode_scores.csv", index_col=0, header=0)
         abs_differences = pd.DataFrame()
         abs_differences["patches"] = all_mode_scores.index
         abs_differences.set_index("patches", inplace=True)
@@ -80,12 +87,73 @@ def process_instance_abs_differences(args):
             abs_differences.at[patch, "mean"] = mean
             abs_differences.at[patch, "std"] = std
         
-        abs_differences.to_csv(f"{EVAL_ROOT}/stability/{test_id}/instances/{inst}/{inst}_abs_differences.csv", index=True, header=True)
+        abs_differences.to_csv(f"{EVAL_ROOT}/stability/{test_id}/{stability_eval_name}/instances/{inst}/{inst}_abs_differences.csv", index=True, header=True)
+
+def compute_global_abs_differences_stats(test_id, instances, stability_eval_name):
+    global_statistics = pd.DataFrame(columns=["Instance"])
+    global_statistics.set_index("Instance", inplace=True)
+    
+    for instance in instances:
+        abs_differences = pd.read_csv(f"{EVAL_ROOT}/stability/{test_id}/{stability_eval_name}/instances/{instance}/{instance}_abs_differences.csv", index_col=0, header=0)
+        global_statistics.at[instance, "Mode Pairs Evaluated"] = len(abs_differences.columns) - 2 # Exclude mean and std columns
+        global_statistics.at[instance, "Global_Mean_Abs_Difference"] = np.mean(abs_differences["mean"])
+        global_statistics.at[instance, "Global_Std_Abs_Difference"] = np.std(abs_differences["mean"])
+        global_statistics.at[instance, "Global_Median_Abs_Difference"] = np.median(abs_differences["mean"])
+        global_statistics.at[instance, "Global_Max_Abs_Difference"] = np.max(abs_differences["mean"])
+        global_statistics.at[instance, "Global_Min_Abs_Difference"] = np.min(abs_differences["mean"])
+    
+    global_statistics.to_csv(f"{EVAL_ROOT}/stability/{test_id}/{stability_eval_name}/abs_differences_global_statistics.csv", index=True, header=True)
+    
+def produce_instances_jaccard_report_parallel(test_id, instances, modes_mapping, mode_pairs, stability_eval_name, logger):
+    rules = ["best", "worst"]
+    ratios = [0.01, 0.05, 0.1, 0.25]
+    
+    for rule in rules:
+        for ratio in ratios:
+            output_path = f"{EVAL_ROOT}/stability/{test_id}/{stability_eval_name}/jaccard_report_{rule}_{ratio}.csv"
+            if not os.path.exists(output_path):
+                args = [(test_id, inst, modes_mapping, mode_pairs, rule, ratio, stability_eval_name) for inst in instances]
+                
+                with mp.Pool(mp.cpu_count()) as pool:
+                    results = pool.map(process_instance_jaccard, args)
+                
+                jaccard_report = pd.DataFrame(columns=["Instance"])
+                jaccard_report["Instance"] = instances
+                jaccard_report.set_index("Instance", inplace=True)
+                for inst, values in results:
+                    for k, v in values.items():
+                        jaccard_report.at[inst, k] = v
+            
+            jaccard_report.to_csv(output_path, index=True, header=True)
+            logger.info(f"Produced Jaccard Similarity Report: {rule}_{ratio}")
+            
+def process_instance_jaccard(args):
+    test_id, inst, modes_mapping, mode_pairs, rule, ratio, stability_eval_name = args
+    
+    all_mode_scores_path = f"{EVAL_ROOT}/stability/{test_id}/{stability_eval_name}/instances/{inst}/{inst}_all_mode_scores.csv"
+    all_mode_scores = pd.read_csv(all_mode_scores_path, index_col=0)
+    total_patches = len(all_mode_scores.index)
+    num_patches = int(total_patches * ratio)
+
+    row = {}
+    for pair in mode_pairs:
+        mode1, mode2 = modes_mapping[pair[0]], modes_mapping[pair[1]]
+        mode1_scores = all_mode_scores[pair[0]]
+        mode2_scores = all_mode_scores[pair[1]]
         
-        final_stats = dict()
-        final_stats["whole_mean"] = np.mean(abs_differences["mean"])
-        final_stats["whole_std"] = np.std(abs_differences["std"])
+        if rule == "best":
+            mode1_sorted = mode1_scores.sort_values(ascending=False)
+            mode2_sorted = mode2_scores.sort_values(ascending=False)
+        else:
+            mode1_sorted = mode1_scores.sort_values(ascending=True)
+            mode2_sorted = mode2_scores.sort_values(ascending=True)
         
-        with open(f"{EVAL_ROOT}/stability/{test_id}/instances/{inst}/{inst}_whole_abs_differences_stats.json", "w") as f:
-            json.dump(final_stats, f, indent=4)
+        mode1_filtered = mode1_sorted.head(num_patches).index.tolist()
+        mode2_filtered = mode2_sorted.head(num_patches).index.tolist()
         
+        intersection = len(set(mode1_filtered) & set(mode2_filtered))
+        union = len(set(mode1_filtered) | set(mode2_filtered))
+        jaccard = intersection / union 
+        row[f"{mode1}X{mode2}"] = jaccard
+ 
+    return inst, row
